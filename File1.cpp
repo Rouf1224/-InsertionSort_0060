934393111f4f8a0e107fee0592c96acf632a81b9
Rouf1224 <ahmadroufr24@gmail.com>
2025-03-23 21:13:03 +0700

    Menambahkan data Array dan variable

--- File1.cpp
+++ File1.cpp
@@ -3,3 +3,11 @@ using namespace std;
 
 int arr[20]; // Membuat Array dengan panjang data 20
 int n; // Membuat Variable inputan n
+
+void input()
+{ // Procedure Input
+    while (true)
+{
+    cout << "Masukan Jumlah Data pada Array : "; // Membuat Inputan jumlah element Array
+    cin >> n; // memanggil variable inputan n
+}}
