1a726682b2dd25f0cbb857f8726853223b4a5be8
Rouf1224 <ahmadroufr24@gmail.com>
2025-03-23 21:52:44 +0700

    Menambahkan data Array dan Variable

--- File1.cpp
+++ File1.cpp
@@ -70,3 +70,12 @@ for (int j = 0; j < n; j++)
     }
     cout << endl; // Output baris kosong
 }
+
+int main()
+{
+    input();
+    insertionSort();
+    display();
+    system("Pause");
+    return 0;
+}
