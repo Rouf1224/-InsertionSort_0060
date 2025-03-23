c81787963b96cccf4463eaf35d0680e443d2a34a
Rouf1224 <ahmadroufr24@gmail.com>
2025-03-23 21:30:56 +0700

    Menambahkan data Array dan Variable

--- File1.cpp
+++ File1.cpp
@@ -20,5 +20,21 @@ void input()
         cout << "\nArray yang anda masukkan maksimal 20 Elemen.\n"; // Menampilkan pesan jika data lebih dari 20
     }
 }
+cout << endl; // Membuat jarak per baris program
+cout << "===================" << endl; // Membuat tampilan susunan data elemen array
+cout << "Masukan Element Array" << endl;
+cout << "===================" << endl;
 
+for (int i = 0; i < n; i++) // Menggunkan perulangan for untuk menyimpan data pada array
+{
+    cout << "Data ke-" << (i + 1) << ": "; // Memasukan atau menginputkan nilai data n
+    cin>> arr[i]; // Menyimpan nilai data n kedalam array arr
+}
+}
+
+void insertionSort()
+{// procedure Insertionsort
+   
+int temp; // Membuat variable data temporer atau penyimpan sementara
+int j, i; // membuat variable j sebagai penanda
 }
